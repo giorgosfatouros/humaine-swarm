6464a77f1234413eef483284b1bae105ca5696f3
George Fatouros <george@alpha-tensor.ai>
2026-09-04 14:48:15 +0300

    feat(starters): add HAIC results and manufacturing machine starter buttons

--- starters.py
+++ starters.py
@@ -1,5 +1,4 @@
 import chainlit as cl
-import random
 
 
 @cl.set_starters
@@ -14,11 +13,20 @@ async def set_starters():
             label="What kind of data have I available?",
             message="What kind of data have I available?",
             icon="/public/s_data.png",
-            ),
+        ),
         cl.Starter(
             label="Check out our ML Pipelines",
             message="Check out our ready-to-use ML Pipelines",
             icon="/public/s_pipeline.png",
-            ),
-       
+        ),
+        cl.Starter(
+            label="My HAIC results",
+            message="List my benchmarking results",
+            icon="/public/s_data.png",
+        ),
+        cl.Starter(
+            label="Manufacturing machines",
+            message="Are there any drilling machines capable of drilling holes larger than 5mm in diameter?",
+            icon="/public/s_pipeline.png",
+        ),
     ]
